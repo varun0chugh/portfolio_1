df16bc9ae34e1d73c0471a0baeb0a75d17cedc19
varun0chugh <138135765+varun0chugh@users.noreply.github.com>
2024-08-23 12:39:20 +0530

    Add files via upload

---
+++ src/components/Projects.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import employeeMSImage from "../assets/port_1.png";
+import bookMSImage from "../assets/mysql_1.png";
+import quizApp from "../assets/quiz_1.png";
+
+const projects = [
+  {
+    id: 1,
+    name: "Portfolio",
+    technologies: "React js",
+    image: employeeMSImage,
+    github: "https://github.com/YouafKhan1",
+  },
+  {
+    id: 2,
+    name: "Quiz App",
+    technologies: "MERN Stack",
+    image: quizApp,
+    github: "https://github.com/YouafKhan1",
+  },
+  {
+    id: 3,
+    name: "Grocery store",
+    technologies: "Flask and Mysql",
+    image: bookMSImage,
+    github: "https://github.com/YouafKhan1",
+  },
+];
+
+const Projects = () => {
+  return (
+    <div className="bg-black text-white py-20" id="project">
+      <div className="container mx-auto px-8 md:px-16 lg:px-24">
+        <h2 className="text-4xl font-bold text-center mb-12">My Projects</h2>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {projects.map((project) => (
+            <div key={project.id} className="bg-gray-800 p-6 rounded-lg hover:shadow-lg 
+            transform transition-transform duration-300 hover:scale-105">
+              <img src={project.image} alt={project.name} className="rounded-lg mb-4 
+              w-full h-48 object-cover" />
+              <h3 className="text-2xl font-bold mb-2">{project.name}</h3>
+              <p className="text-gray-400 mb-4">{project.technologies}</p>
+              <a href={project.github} className="inline-block bg-gradient-to-r 
+              from-green-400 to-blue-500 text-white px-4 py-2 rounded-full" target="_blank" 
+              rel="noopener noreferrer">GitHub</a>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
